357c8754656774e1b5afbe91a4417e288a95623a
ThupiliSaikiran <thupilisaikiran11@gmail.com>
2025-06-19 01:24:47 +0530

    Trigger redeploy for Render to pick up runtime.txt

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate
-python manage.py create_superuser --mobile 998932044 --name "Sreenu" --password "sr123"
+python manage.py createsuperuser --mobile 9989320447 --name "Sreenu" --password "sr123" --noinput || true
 python manage.py collectstatic --noinput
 gunicorn village_power.wsgi:application
